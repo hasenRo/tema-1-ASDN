1b32b26334a68e98ec2a40b2eddec0fa87446bad
hasenRo <hanoonhussen@gmail.com>
2026-03-16 15:53:34 +0200

    Add header file for base 3 conversion functions

---
+++ Baza 3 in Zecima Si Invers /functii.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef _FUNCTII_H_
+#define _FUNCTII_H_
+
+int baza3_InZecimal(char* base3);
+
+char* Zecimal_InBaza3(int base10);
+
+void swap(char* a, char* b);
+
+#endif
